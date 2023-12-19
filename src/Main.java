b6749b6ca711e474cbaccbd43b34827369fc8950
Rakhman <231355@astanait.edu.kz>
2023-12-20 00:21:30 +0600

    added print massege

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++);
-
-        }
-        System.out.println();
-        System.out.println("hello world");
-
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        System.out.println(a);
     }
 }
